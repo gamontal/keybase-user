4c6e4cff9cb2555f8a116be374650492da5c227e
Gabriel Montalvo <gabe.mon5@outlook.com>
2015-12-24 18:33:08 -0400

    username and string validation bug fix

--- index.js
+++ index.js
@@ -1,44 +1,51 @@
 var Promise = require('bluebird');
+
 module.exports =  function(v, p) {
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var httpGet = function(url) {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open("GET", url, false);
-        xmlHttp.send(null);
-        return JSON.parse(xmlHttp.responseText);
-    };
+  var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+  var xmlHttp;
+  var httpGet = function(url) {
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open('GET', url, false);
+    xmlHttp.send(null);
+    return JSON.parse(xmlHttp.responseText);
+  };
 
-    var url = 'https://keybase.io/_/api/1.0/user/lookup.json?', res, req;
-    var val = v, proof;
+  var url = 'https://keybase.io/_/api/1.0/user/lookup.json?', res, req;
+  var val = v, proof;
 
-    if (p === undefined) {
-        proof  = 'username';
-    } else {
-        proof = p;
+  if (p === undefined) {
+    proof  = 'username';
+  } else {
+    proof = p;
+  }
+
+  return new Promise(function (resolve, reject) {
+    if ((typeof v !== 'string') || (v === undefined)) {
+      return Promise.reject(new Error('username required'));
     }
 
-    return new Promise(function (resolve, reject) {
-        req = {
-            id: httpGet(url + proof + '=' + val),
-            basics: httpGet(url + proof + '=' + val + '&fields=basics'),
-            profile: httpGet(url + proof + '=' + val + '&fields=profile'),
-            public_keys: httpGet(url + proof + '=' + val + '&fields=public_keys'),
-            crypto_add: httpGet(url + proof + '=' + val + '&fields=cryptocurrency_addresses')
-        };
+    req = {
+      id: httpGet(url + proof + '=' + val),
+      basics: httpGet(url + proof + '=' + val + '&fields=basics'),
+      profile: httpGet(url + proof + '=' + val + '&fields=profile'),
+      public_keys: httpGet(url + proof + '=' + val + '&fields=public_keys'),
+      crypto_add: httpGet(url + proof + '=' + val + '&fields=cryptocurrency_addresses')
+    };
 
-        if (req.id.them === undefined) {
-            return resolve(null);
-        } else if ((req.id.them === undefined) && (req.id.them[0] === undefined)) {
-            return resolve(null);
-        } else {
-            res = {
-                id: req.id.them.id || req.id.them[0].id,
-                basics: req.basics.them.basics || req.basics.them[0].basics,
-                profile: req.profile.them.profile || req.profile.them[0].profile,
-                public_keys: req.public_keys.them.public_keys || req.public_keys.them[0].public_keys,
-                crypto_add: req.crypto_add.them.cryptocurrency_addresses || req.crypto_add.them[0].cryptocurrency_addresses
-            };
-            return resolve(res);
-        }
-    });
+    if (req.id.them === undefined) {
+      return resolve(null);
+    } else if ((req.id.them === undefined) && (req.id.them[0] === undefined)) {
+      return resolve(null);
+    } else {
+      res = {
+        id: req.id.them.id || req.id.them[0].id,
+        basics: req.basics.them.basics || req.basics.them[0].basics,
+        profile: req.profile.them.profile || req.profile.them[0].profile,
+        public_keys: req.public_keys.them.public_keys || req.public_keys.them[0].public_keys,
+        crypto_add: req.crypto_add.them.cryptocurrency_addresses || req.crypto_add.them[0].cryptocurrency_addresses
+      };
+      return resolve(res);
+    }
+  });
 };
+
--- test.js
+++ test.js
@@ -2,25 +2,24 @@ var kbuser = require('./');
 
 var usr1, usr2;
 kbuser('chris').then(function(user) {
-    usr1 = {
-        id: user.id,
-        basics: user.basics,
-        profile: user.profile,
-        public_keys: user.public_keys,
-        crypto_address: user.crypto_add,
-        all: user
-    };
+  usr1 = {
+    id: user.id,
+    basics: user.basics,
+    profile: user.profile,
+    public_keys: user.public_keys,
+    crypto_address: user.crypto_add,
+    all: user
+  };
 });
 
 kbuser('bitcoyne', 'coinbase').then(function(user) {
-    usr2 = {
-        id: user.id,
-        basics: user.basics,
-        profile: user.profile,
-        public_keys: user.public_keys,
-        crypto_address: user.crypto_add,
-        all: user
-    };
+  usr2 = {
+    id: user.id,
+    basics: user.basics,
+    profile: user.profile,
+    public_keys: user.public_keys,
+    crypto_address: user.crypto_add,
+    all: user
+  };
 });
-
 
